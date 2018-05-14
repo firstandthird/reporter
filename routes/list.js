f320f3b34fe60e677cebbbd13feb75948286b71c
chris <orthagonal1@gmail.com>
2018-05-14 15:14:46 -0500

    list

---
+++ routes/list.js
@@ -0,0 +1,28 @@
+// returns a table listing all registered reports:
+exports.routes = {
+  method: 'GET',
+  path: '/',
+  config: {
+    plugins: {
+      'hapi-transform-table': {}
+    }
+  },
+  handler(request, h) {
+    const server = request.server;
+    let base = server.settings.app.routePrefix ?
+      `${request.info.host}/${server.settings.app.routePrefix}`
+      : `${request.info.host}`;
+    base = request.url.host === 'https' ? `https://${base}` : `http://${base}`;
+    const links = [];
+    Object.keys(server.methods.reports).forEach(key => {
+      const root = `${base}/${key}`;
+      links.push({
+        name: key,
+        csv: `${root}.csv`,
+        html: `${root}.html`,
+        json: `${root}.json`,
+      });
+    });
+    return h.response(links);
+  }
+};
